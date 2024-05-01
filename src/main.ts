56659ee4dc6899eb56f8abd1da452de9ee2e014d
Cristian Rivadero <cristiandeveloper20@protonmail.com>
2024-05-01 17:42:34 -0300

    add in main.ts the SwaggerModule and configure it.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
     transform: true,
     forbidNonWhitelisted: true,
   }));
+  const config = new DocumentBuilder()
+    .setTitle("API REST example of users, profiles, posts and comments")
+    .setDescription("Nest.js REST API for user profiles, posts, and comments. Users create profiles, share posts, and engage through comments.")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
